5a0d7aa49162e49061bec8aec242707e8e1249f2
Yannick Allusse <yallusse@ltutech.com>
2021-11-18 15:24:09 +0100

    try to fix an package error arround ipympl

--- notebooks/start_notebooks.sh
+++ notebooks/start_notebooks.sh
@@ -2,4 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# TODO: I do not know why now, but the version installed in the docker does not work properly.
+#       We need to reinstall it here.
+pip install ipympl
 jupyter notebook --allow-root --port=2347 --ip=0.0.0.0 --no-browser --NotebookApp.default_url=notebooks/notebooks/welcome.ipynb
